6f8b52e9f7f3c9c106dfaafc57fe4dc80b6539a1
snixie <fsd.rabgay@gmail.com>
2023-07-04 21:38:22 +0600

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/user/login/login.component';
+import { RegisterComponent } from './pages/user/register/register.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TaskCreateComponent } from './pages/task/task-create/task-create.component';
+import { TaskListComponent } from './pages/task/task-list/task-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    TaskCreateComponent,
+    TaskListComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
